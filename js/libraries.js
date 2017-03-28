c00f4d7b4a5200b8854f57379237b4e4d9265d49
Jordan Kamin <jordanakamin@gmail.com>
2017-03-28 15:56:08 -0700

    adds more syllables to name library

--- js/libraries.js
+++ js/libraries.js
@@ -1,8 +1,8 @@
 const libraries = {
   name: [
     ['To', 'Be', 'Jo', 'Am', 'Da', 'Wo', 'El', 'Al', 'Pa', 'Ra', 'Xa', 'Re', 'Wa', 'Uk', 'Og', 'Po', 'Di', 'Fi', 'Iz', 'Zo'],
-    ['re', 'orn', 'bi', 'shi', 'ru', 'ha', 'da', 'he', 'la', 'x', 'jo', '', 'han', 'ga', 'no', 'vi', 'le', 'qi', 'ex', 's'],
-    ['lan', 'tar', 'rf', 'shi', 'ji', 'ro', 'en', 'an', 'do', 'ti', '', 'dyn', 'wyn', 'ia', 'sha', 'ko', 'ka', 'ki', 'ta', 'nil']
+    ['re', 'orn', 'bi', 'shi', 'ru', 'ha', 'da', 'he', 'la', 'x', 'jo', '', 'han', 'ga', 'no', 'vi', 'le', 'qi', 'ex', 's', 'bb', 'cc', 'k', 'ck', 'ia'],
+    ['lan', 'tar', 'rf', 'shi', 'ji', 'ro', 'en', 'an', 'do', 'ti', '', 'dyn', 'wyn', 'ia', 'sha', 'ko', 'ka', 'ki', 'ta', 'nil', 'yn', 'ugh', 'gh', 'ach', 'et', 'ar', 'lin', 'lyn', 'on', 'am', 's' ]
   ],
   epicName: [
     ['Storm', 'Flame', 'Ice', 'Wind', 'Wood', 'Shadow', 'Night', 'Day', 'Light', 'Mountain', 'Rock', 'Sea', 'Snow', 'Cave'],
