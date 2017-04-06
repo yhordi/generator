42ad39d56d9251dfb394dc8b1c1b5d6a43f53820
Jordan Kamin <jordanakamin@gmail.com>
2017-04-06 14:05:59 -0700

    builds sidebar toggle functionality

--- js/libraries.js
+++ js/libraries.js
@@ -1,8 +1,8 @@
 const libraries = {
   name: [
     ['To', 'Be', 'Jo', 'Am', 'Da', 'Wo', 'El', 'Al', 'Pa', 'Ra', 'Xa', 'Re', 'Wa', 'Uk', 'Og', 'Po', 'Di', 'Fi', 'Iz', 'Zo'],
-    ['re', 'orn', 'bi', 'shi', 'ru', 'ha', 'da', 'he', 'la', 'x', 'jo', '', 'han', 'ga', 'no', 'vi', 'le', 'qi', 'ex', 's', 'bb', 'cc', 'k', 'ck', 'ia'],
-    ['lan', 'tar', 'rf', 'shi', 'ji', 'ro', 'en', 'an', 'do', 'ti', '', 'dyn', 'wyn', 'ia', 'sha', 'ko', 'ka', 'ki', 'ta', 'nil', 'lil', 'yn', 'ugh', 'gh', 'ach', 'et', 'ar', 'lin', 'lyn', 'on', 'am', 's', 'han', 'had', 'has' ]
+    ['re', 'orn', 'bi', 'shi', 'ru', 'ha', 'da', 'he', 'la', 'x', 'jo', '', 'han', 'ga', 'no', 'vi', 'le', 'qi', 'ex', 's', 'sh', 'cc', 'k', 'ck', 'ia', 'a', 'e', 'i', 'o', 'u'],
+    ['lan', 'tar', 'rf', 'shi', 'ji', 'ro', 'en', 'an', 'do', 'ti', '', 'dyn', 'wyn', 'ia', 'sha', 'kon', 'con', 'ko', 'ka', 'ki', 'ta', 'nil', 'lil', 'yn', 'ugh', 'gh', 'ach', 'et', 'ar', 'lin', 'lyn', 'on', 'am', 's', 'han', 'had', 'has' ]
   ],
   epicName: [
     ['Storm', 'Flame', 'Ice', 'Wind', 'Wood', 'Shadow', 'Night', 'Day', 'Light', 'Mountain', 'Rock', 'Sea', 'Snow', 'Cave'],
--- js/main.js
+++ js/main.js
@@ -1,14 +1,35 @@
 import { generateName } from './generator'
 
 let $firstName = $('#first-name')
+let $epic = $('#epic-name')
+let $nick = $('#nickname')
+let $sidebar = $('.sidebar')
+
 $firstName.on("click", function(){
   $('#first-name-target').text(generateName('name'))
 })
-let $epic = $('#epic-name')
 $epic.on("click", function(){
   $('#epic-name-target').text(generateName('epicName'))
 })
-let $nick = $('#nickname')
 $nick.on("click", function(){
   $('#nickname-target').text('The ' +generateName('nickname'))
 })
+$sidebar.on("click", function(){
+  // $sidebar.removeClass('offscreen')
+  // if(!clicked) {
+  //   let clicked;
+  // }
+  $sidebar.toggleClass('offscreen')
+  sidebarSwitch()
+})
+
+let sidebarSwitch = () => {
+  // if(clicked == true) {
+  //   $sidebar.removeClass('offscreen')
+  //   let clicked = false
+  // } else {
+  //   let clicked = true
+  //   $sidebar.addClass('offscreen')
+  // }
+
+}
