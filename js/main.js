7ebdd53525c5c9695d5a9e59d885338fa25d7e57
Jordan Kamin <jordanakamin@gmail.com>
2017-04-07 15:01:35 -0700

    builds light dismiss

--- js/main.js
+++ js/main.js
@@ -1,19 +1,34 @@
 import { generateName } from './generator'
 
-let $firstName = $('#first-name')
-let $epic = $('#epic-name')
-let $nick = $('#nickname')
-let $tab = $('#tab')
+let $firstName = $('#first-name');
+let $epic = $('#epic-name');
+let $nick = $('#nickname');
+let $tab = $('#tab');
 
 $firstName.on("click", function(){
   $('#first-name-target').text(generateName('name'))
-})
+});
 $epic.on("click", function(){
   $('#epic-name-target').text(generateName('epicName'))
-})
+});
 $nick.on("click", function(){
   $('#nickname-target').text('The ' + generateName('nickname'))
-})
+});
 $tab.on("click", function(){
+  toggleSidebar()
+});
+
+let sidebarClick = () => {
+  if($('.offscreen').length != 0) {
+    $('.content').on('click', function(){
+      toggleSidebar()
+    });
+  } else {
+    $('.content').unbind()
+  }
+};
+
+let toggleSidebar = () => {
+  sidebarClick()
   $('.sidebar').toggleClass('offscreen')
-})
+};
