6390384305161c4604ff376a0a57c85a31229bba
Jordan Kamin <jordanakamin@gmail.com>
2017-04-10 15:11:53 -0700

    WIP building sidebar listeners

--- js/main.js
+++ js/main.js
@@ -4,29 +4,50 @@ let $firstName = $('#first-name');
 let $epic = $('#epic-name');
 let $nick = $('#nickname');
 let $tab = $('#tab');
+$('.menu-item').on('click', function(e){
+  e.preventDefault();
+  this.dataset.key
+  debugger
+});
+// $firstName.on("click", function(){
+//   $('#first-name-target').text(generateName('name'))
+// });
+// $epic.on("click", function(){
+//   $('#epic-name-target').text(generateName('epicName'))
+// });
+// $nick.on("click", function(){
+//   $('#nickname-target').text('The ' + generateName('nickname'))
+// });
 
-$firstName.on("click", function(){
-  $('#first-name-target').text(generateName('name'))
-});
-$epic.on("click", function(){
-  $('#epic-name-target').text(generateName('epicName'))
-});
-$nick.on("click", function(){
-  $('#nickname-target').text('The ' + generateName('nickname'))
-});
+$('#generate').on('click', function(e){
+  e.preventDefault()
+  console.log(e)
+})
+
 $tab.on("click", function(){
   toggleSidebar()
 });
 
+$('body').on('click', function(e){
+  if(!$(e.target).closest('.sidebar')) {
+    hideSidebar()
+  }
+});
+
+let nameOptions = {}
+
+
 let sidebarClick = () => {
   if($('.offscreen').length != 0) {
-    $('.content').on('click', function(){
-      toggleSidebar()
-    });
+
   } else {
     $('.content').unbind()
   }
 };
+
+let hideSidebar = () => {
+  $('.sidebar').addClass('offscreen')
+}
 
 let toggleSidebar = () => {
   sidebarClick()
