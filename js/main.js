1dd3a15ae1d4378b74da329886dc839fa8c87c09
Jordan Kamin <jordanakamin@gmail.com>
2017-05-01 11:37:15 -0700

    trash showing and hiding correctly

--- js/main.js
+++ js/main.js
@@ -5,46 +5,54 @@ let $epic = $('#epic-name');
 let $nick = $('#nickname');
 let $tab = $('#tab');
 let keys = [];
+
 $('.menu-item').on('click', function(e){
   e.preventDefault();
   let $listItem = $(this).parent()
   $listItem.toggleClass('pressed');
   toggleKey($(this))
-  renderTrash($listItem)
+  checkContent()
+  toggleTrash($listItem)
 });
 
-let renderTrash = (listItem) => {
-  if(listItem.attr('class') == "pressed") {
-    listItem.append('<span class="fa fa-trash"></span>')
-  } else {
-    listItem.children('span').remove()
-  }
+let toggleTrash = (listItem) => {
+  listItem.children('span').toggleClass('hidden')
 };
+
+let checkContent = () => {
+  let $targets = $('.content span')
+  $.each($targets, (index, target) =>{
+    let link = $("a[data-key='" + target.dataset.key + "']")
+    if(target.textContent.length > 0) {
+      link.siblings().toggleClass('hidden')
+    }
+  });
+}
 
 let bindGenerate = () => {
   $('#generate').on('click', (e) => {
-    e.preventDefault()
+    e.preventDefault();
     getKeys().forEach((key)=> {
       $('#'+ key +'-name-target').html(generateName(key))
-    })
-  })
+    });
+  });
 }
 
 $tab.on("click", function(){
-  toggleSidebar()
+  toggleSidebar();
 });
 
 $('body').on('click', function(e){
   if(!$(e.target).closest('.sidebar')) {
-    hideSidebar()
+    hideSidebar();
   }
 });
 
 let toggleKey = ($this) => {
   if($this.attr('data-toggle') == 'true') {
-    $this.removeAttr('data-toggle')
+    $this.removeAttr('data-toggle');
   } else {
-    $this.attr('data-toggle', 'true')
+    $this.attr('data-toggle', 'true');
   }
 }
 
@@ -53,7 +61,7 @@ let getKeys = () => {
   let $links = $('.menu li a')
   $.each($links, (index, link) => {
     if($(link).attr('data-toggle') == 'true'){
-      keys.push(link.dataset["key"])
+      keys.push(link.dataset["key"]);
     }
   });
   return keys;
